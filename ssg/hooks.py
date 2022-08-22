c885455372acfea704574584b23dd692fbf80f2b
Chris Tant <ctant3@ford.com>
2022-08-22 11:46:28 -0400

    Completed module

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -2,6 +2,7 @@ _callbacks = {}
 
 def register(hook, order=0):
     def register_callback(func):
+        setdefault(hook, {}).setdefault(order, []).append(func)
         return func
 
     return register_callback
