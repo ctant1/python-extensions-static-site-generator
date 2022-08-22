90030aae6a4916dc46cf0e8977661c335edd5177
Chris Tant <ctant3@ford.com>
2022-08-22 11:53:46 -0400

    Completed module

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -9,6 +9,6 @@ def register(hook, order=0):
 
 def event(hook, *args):
     for order in sorted(_callbacks.get(hook, {})):
-        for func _callbacks[hook][order]:
+        for func in _callbacks[hook][order]:
             func(*args)
 
