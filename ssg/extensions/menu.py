4156a9f83ef5fc121ec66e43503157299abf6d4d
Chris Tant <ctant3@ford.com>
2022-08-23 08:19:39 -0400

    Completed module

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -1,3 +1,7 @@
 from ssg import hooks, parsers
 
 files = []
+
+def collect_files(source, site_parsers):
+    hooks.register(collect_files)
+    valid = lambda p:  False if (isinstance(p) == parsers.ResourceParser) else True
